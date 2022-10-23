b836fd5478e7a23066eade6052a7e672ee7d4f51
Rainbow Harrison <rainbow@spadget.com>
2022-10-23 08:48:33 +0100

    Adding js back in

--- 404.js
+++ 404.js
@@ -1,26 +1,42 @@
-// var h = document.getElementById("page").scrollHeight;
+var h = document.getElementById("page").scrollHeight;
 
-// var pad = 48;
-// const mql = window.matchMedia('(max-width: 1200px)');
+var pad = 48;
+const mql = window.matchMedia('(max-width: 1200px)');
 
-// function screenTest(e) {
-//   if (e.matches) {
-//     pad = 78;
-//   } else {
+function screenTest(e) {
+  if (e.matches) {
+    pad = 78;
+  } else {
 
-//   }
-// }
+  }
+}
 
-// mql.addEventListener('change', screenTest);
+mql.addEventListener('change', screenTest);
 
 
-// var full = h + pad;
+var full = h + pad;
 
-// let num = full;
-// let fulltext = num.toString();
+let num = full;
+let fulltext = num.toString();
 
-// var px = "px"
+var px = "px"
 
-// var fullpx = fulltext + px
+var fullpx = fulltext + px
 
-// document.getElementById("html").style.height = fullpx;
+document.getElementById("html").style.height = fullpx;
+
+// (function(){location.reload()}, 3000);
+
+setTimeout(function()
+{
+  if( window.localStorage )
+  {
+    if(!localStorage.getItem('firstLoad'))
+    {
+      localStorage['firstLoad'] = true;
+      window.location.reload();
+    }  
+    else
+      localStorage.removeItem('firstLoad');
+  }
+}, 50)();
